c70efc4d4b7753aac61da917069a2772770972b2
Sahil Daronde <sahildaronde9@gmail.com>
2025-11-21 22:36:43 +0530

    Sorted HashMap based on keys

---
+++ CollectionDemo/src/HashMapDemo.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+import java.util.TreeMap;
+
+public class HashMapDemo {
+    public static void main(String[] args) {
+
+        HashMap <Integer, String> map = new HashMap<>();
+        map.put(20, "Rohit");
+        map.put(50, "Nayan");
+        map.put(80, "Rohan");
+        map.put(10, "Bhushan");
+        map.put(40, "Vishal");
+
+        System.out.println("Without shorting>> "+map);
+
+        TreeMap <Integer, String> treeMap = new TreeMap<>(map);
+        System.out.println(treeMap);
+    }
+}
